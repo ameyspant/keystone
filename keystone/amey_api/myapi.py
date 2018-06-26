3e6d8ee6aa86b7e07531410957a02aa77fc0e22f
ameyspant <ameyspant@gmail.com>
2018-06-26 16:26:24 +0530

    Update myapi.py

--- keystone/amey_api/myapi.py
+++ keystone/amey_api/myapi.py
@@ -1,8 +1,21 @@
 import keystone.conf
+from keystone.common import controller
+from oslo_log import log
 
 
+
+
+LOG = log.getLogger(__name__)
 CONF = keystone.conf.CONF
 
 
-def list_data():
-    return {{'host': CONF.amey.host},{'port': CONF.amey.port}}
+class Amey(controller.V3Controller):
+    collection_name = 'amey_api'
+    member_name = 'amey'
+    def __init__():
+        pass
+    @controller.protected()
+    def list_data(self, request):
+        return {{'host': CONF.amey.host},{'port': CONF.amey.port}}
+
+LOG.debug("Debug")
