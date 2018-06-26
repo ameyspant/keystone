6dc25e96a6c48d4f8d08529e7b65271747e5477c
ameyspant <ameyspant@gmail.com>
2018-06-26 16:27:05 +0530

    Update routers.py

--- keystone/amey_api/routers.py
+++ keystone/amey_api/routers.py
@@ -1,12 +1,11 @@
 from keystone.amey_api import myapi
-from keystone.common import json_home
 from keystone.common import wsgi
 
 
 class Routers(wsgi.RoutersBase):
 
     def append_v3_routers(self, mapper, routers):
-        amey_controller = myapi.list_data()
+        amey_controller = myapi.Amey()
 
         self._add_resource(
             mapper, amey_controller,
