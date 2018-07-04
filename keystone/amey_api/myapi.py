1ad738c3534cc6c0725286027d5b01d7e53f6240
ameyspant <ameyspant@gmail.com>
2018-07-04 01:22:12 -0500

    modified api to show current user,project,domain,role

--- keystone/amey_api/myapi.py
+++ keystone/amey_api/myapi.py
@@ -2,14 +2,16 @@ import keystone.conf
 from keystone.common import controller
 from keystone.common import wsgi
 from oslo_log import log
+from keystone.identity import controllers
 
+from keystone.common import dependency
 
 
 
 LOG = log.getLogger(__name__)
 CONF = keystone.conf.CONF
 
-
+@dependency.requires('identity_api', 'assignment_api')
 class Amey(controller.V3Controller):
     collection_name = 'amey_api'
     member_name = 'myapi'
@@ -18,11 +20,17 @@ class Amey(controller.V3Controller):
 
     @controller.protected()
     def list_data(self, request):
-        #return ('amey')
-        #data = dict()
-        #data[0] = "amey"
-        data={'host': CONF.amey.host,'port': CONF.amey.port}
+        user_id = request.auth_context.get('user_id')
+
+        ref = self.identity_api.get_user(user_id)
+        roles = self.assignment_api.list_role_assignments(user_id=user_id,effective=True)
+        user_refs = self.assignment_api.list_projects_for_user(user_id)
+        domain_refs = self.assignment_api.list_domains_for_user(user_id)
+        data = {'user': ref,'project': user_refs,'domain':domain_refs,'role':roles}
         return wsgi.render_response(body=data)
+
+        #data =  {'user': self.v3_to_v2_user(ref)}
+        #return wsgi.render_response(body=data)
         #return wsgi.render_response(body = data)
 LOG.debug("Debug")
 
